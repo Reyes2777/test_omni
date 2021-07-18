24031fcbccadabff7b9420cdcfac4a5d6e574960
Jonathan Reyes <jonathan.reyes2777gmail.com>
2021-07-18 16:40:14 -0500

    DELETE: remove package.sh, file no ussing in this app

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install gunicorn gcc
-apt-get -y install git
-apt-get clean
-rm -rf /var/lib/apt/lists/*
